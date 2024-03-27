4692128f5874a4a203ca16e64cdb1ac12cdf2857
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2024-03-27 01:40:20 -0400

    feat: Add a Sample Description to Each Destination For Testing

--- src/routes/seed/index.ts
+++ src/routes/seed/index.ts
@@ -36,6 +36,7 @@ router.put("/", async (req, res, next) => {
       destinations.map((destination) => ({
         name: destination.city,
         country: destination.country,
+        description: "This is a sample description.",
       }))
     );
 
