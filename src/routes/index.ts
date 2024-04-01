e00dd4440567e886e849331ff2e8634e8aadbe39
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2024-04-01 16:44:51 -0400

    refactor: Rename Destinations Route to Home

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import auth from "./auth";
-import destinations from "./destinations";
+import home from "./home";
 import destination from "./destination";
 import seed from "./seed";
 import search from "./search";
@@ -9,9 +9,9 @@ import explore from "./explore";
 const router = Router();
 
 router.use("/auth", auth);
-router.use("/destination", destination);
-router.use("/destinations", destinations);
 router.use("/seed", seed);
+router.use("/destination", destination);
+router.use("/home", home);
 router.use("/search", search);
 router.use("/user", user);
 router.use("/explore", explore);
